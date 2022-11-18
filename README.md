e2bd52871938ae594eb6fb037bd15c2e0e424ac3
madsbertelsen <bertelsenschmidt@gmail.com>
2022-11-18 08:34:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-native-web-starter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-native-web-starter-ff2zc2)
---
+++ index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { render, View, Text, StyleSheet } from 'react-native-web';
+import Hello from './hello';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Hello name={this.state.name} />
+        <Text>
+          Start editing to see some magic happen :)
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  }
+})
+
+render(<App />, document.getElementById('root'));
