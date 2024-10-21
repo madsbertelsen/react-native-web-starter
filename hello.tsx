d230de60d2c19dc1e2c637b98655fa046bdfdce4
madsbertelsen <bertelsenschmidt@gmail.com>
2024-10-21 10:32:51 +0200

    1 file modified

--- hello.tsx
+++ hello.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Text, StyleSheet, View, Image } from 'react-native-web';
+import { useLivePreview } from '@payloadcms/live-preview-react';
 
-export default ({ name }: { name: string }) => (
-  <View>
-    <Image
-      style={{ height: 100, width: '100%' }}
-      source={{
-        uri: 'https://payload.demo.mapstory.io/api/media/file/Cover%20i%20APP,%20Sallingsund%20-%201000x200.jpg',
-      }}
-    />
-  </View>
-);
+export default ({ name }: { name: string }) => {
+  const { data, isLoading } = useLivePreview({
+    initialData: null,
+
+    serverURL: 'https://payload.demo.mapstory.io',
+    depth: 2,
+  });
+  if (data) {
+    alert(data);
+  }
+  return (
+    <View>
+      <Image
+        style={{ height: 100, width: '100%' }}
+        source={{
+          uri: 'https://payload.demo.mapstory.io/api/media/file/Cover%20i%20APP,%20Sallingsund%20-%201000x200.jpg',
+        }}
+      />
+      <Text>{JSON.stringify(data, null, 2)}</Text>
+    </View>
+  );
+};
