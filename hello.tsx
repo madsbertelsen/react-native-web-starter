0aade96803674687064200602615a74f6daeb053
madsbertelsen <bertelsenschmidt@gmail.com>
2024-10-21 09:02:49 +0200

    1 file modified

--- hello.tsx
+++ hello.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native-web';
 
-export default ({ name }) => <Text style={styles.text}>Hello {name}!</Text>;
+export default ({ name }: { name: string }) => (
+  <Text style={styles.text}>Hello {name}!</Text>
+);
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 8,
-  }
-})
+  },
+});
